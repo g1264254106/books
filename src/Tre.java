c7a2446b41957274a23f09bc1530ded038177986
GuoHaojie <1264254106@qq.com>
2023-10-12 14:34:15 +0800

    master第五次提交

--- src/Tre.java
+++ src/Tre.java
@@ -6,5 +6,6 @@ public class Tre {
         System.out.println("999");
         System.out.println("mastermastermaster");
         System.out.println("devdevdevdev");
+        System.out.println("111111111111");
     }
 }
